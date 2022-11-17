7256171ef2e8772c5c515d8813af88cee95b5efb
adiv1kram <adityavikram250201@gmail.com>
2022-11-17 17:19:38 +0530

    daily update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 'use strict';
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.number').textContent=13;
-document.querySelector('.score').textContent=01313;
+document.querySelector('.score').textContent=0113;
